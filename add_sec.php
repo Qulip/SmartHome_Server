99db1621bf124a99e59c32226d1992fd5c8a5b96
Qulip <77991314+Qulip@users.noreply.github.com>
2021-09-27 23:51:10 +0900

    Add secure add, getjson.php

---
+++ add_sec.php
@@ -0,0 +1,17 @@
+<?php 
+  header("Content-Type: text/html;charset=UTF-8"); 
+  require 'connect_sql.php';
+  $mysqli = connect_sql(); //sql과 연결
+    if($mysqli){ 
+      echo "MySQL successfully connected!<br/>"; //연결 확인 후 POST로 값 받아오기
+      $loc = $_POST['loc']; 
+      
+      echo "<br/>loc = $loc";  //확인차 값 확인 후 sql에 저장
+      $query = "INSERT INTO secure (temp) VALUES ('$loc')"; 
+      mysqli_query($mysqli,$query); 
+      echo "</br>success!!"; 
+    } else{ //sql에 연결 실패했을경우
+      echo "MySQL could not be connected"; 
+      } 
+    mysqli_close($mysqli); 
+?>
