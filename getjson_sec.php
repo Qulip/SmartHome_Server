eaef410321dddb6af9098ab9b06df37e1813f11b
Qulip <77991314+Qulip@users.noreply.github.com>
2021-11-29 05:30:18 +0900

    delete soundcode and change comments

--- getjson_sec.php
+++ getjson_sec.php
@@ -3,10 +3,10 @@
     $mysqli = connect_sql();
 
     if ($mysqli) { 
-        mysqli_set_charset($mysqli,"utf8"); // 기본 클라이언트 문자 집합 설정하기 
+        mysqli_set_charset($mysqli,"utf8");                   // 기본 클라이언트 문자 집합 설정하기 
         $res = mysqli_query($mysqli, "select * from secure"); // 쿼리문 실행, 결과를 res에 저장 
-        $result = array(); // 결과를 배열로 변환하기 위한 변수 정의 
-        while($row = mysqli_fetch_array($res)) { // 쿼리문의 결과(res)를 배열형식으로 변환(result) 
+        $result = array();                                    // 결과를 배열로 변환하기 위한 변수
+        while($row = mysqli_fetch_array($res)) {              //결과를 배열형식으로 변환
             $temp = array(
                 'reg_time'=>$row[0],
                 'loc'=>$row[1]);
